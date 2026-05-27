b6789312439761065b897e95a525adc109790ea0
Ranjeet1228 <upadhyayr0728@gamil.com>
2026-05-27 17:39:33 +0530

    push item

---
+++ Admin/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main {
+
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        String information = """
+                Press 1:- for loading infomation;
+                Press 2: - for adding book into the library
+                press 3: - to assign the book to any student
+                """;
+        System.out.println(information);
+    }
+}
---
+++ FolderName/Management.java
@@ -0,0 +1,4 @@
+package FolderName;
+
+public class Management {
+}
